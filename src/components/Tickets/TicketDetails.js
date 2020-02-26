aaea464c5f9b0fd63196d154fff3dbc424084c6f
Shashank Neelagiri <sneelagiri@outlook.com>
2020-02-26 18:55:37 +0100

    added fraud risk algorithim

--- src/components/Tickets/TicketDetails.js
+++ src/components/Tickets/TicketDetails.js
@@ -5,6 +5,92 @@ import "./tickets.css";
 import Comments from "./Comments";
 class TicketDetails extends Component {
   render() {
+    function round_to_precision(x, precision) {
+      var y = +x + (precision === undefined ? 0.5 : precision / 2);
+      return y - (y % (precision === undefined ? 1 : +precision));
+    }
+    // Risk analysis logic stars
+    let numComments = 0;
+    this.props.comments.map(user => {
+      return user.comments.map(comment => {
+        if (comment.ticketId == this.props.match.params.ticketId) {
+          numComments++;
+          console.log("Number of Comments", numComments);
+        }
+      });
+    });
+    let numTickets = 0;
+    let sumPrice = 0;
+    this.props.tickets.map(user => {
+      return user.tickets.map(ticket => {
+        if (ticket.eventId == this.props.match.params.eventId) {
+          numTickets++;
+          sumPrice = sumPrice + ticket.price;
+          return ticket;
+        } else {
+          return ticket;
+        }
+      });
+    });
+    const minRisk = 5;
+    const maxRisk = 95;
+    let risk = 0;
+    const averagePrice = sumPrice / numTickets;
+    this.props.tickets.map(user => {
+      return user.tickets.map(ticket => {
+        if (
+          ticket.id == this.props.match.params.ticketId &&
+          user.tickets.length === 1
+        ) {
+          risk = risk + 10;
+          if (numComments > 3) {
+            risk = risk + 5;
+          }
+          const time = parseInt(ticket.updatedAt.slice(11, 13));
+          if (time > 9 && time < 17) {
+            risk = risk - 10;
+          } else {
+            risk = risk + 10;
+          }
+          const priceDifference = ticket.price - averagePrice;
+          if ((priceDifference > 0) & (priceDifference < 10)) {
+            risk = risk - priceDifference;
+          } else if (priceDifference > 10) {
+            risk = risk - 10;
+          } else {
+            risk = risk - priceDifference;
+          }
+          return ticket;
+        } else if (ticket.id == this.props.match.params.ticketId) {
+          if (numComments > 3) {
+            risk = risk + 5;
+          }
+          const time = parseInt(ticket.updatedAt.slice(11, 13));
+          if (time > 9 && time < 17) {
+            risk = risk - 10;
+          } else {
+            risk = risk + 10;
+          }
+
+          const priceDifference = ticket.price - averagePrice;
+          if ((priceDifference > 0) & (priceDifference < 10)) {
+            risk = risk - priceDifference;
+          } else if (priceDifference > 10) {
+            risk = risk - 10;
+          } else {
+            risk = risk - priceDifference;
+          }
+          return ticket;
+        } else {
+          return null;
+        }
+      });
+    });
+    risk = round_to_precision(risk, 0.01);
+    // Based on number of comments:
+
+    console.log(averagePrice);
+    // Risk analysis logic ends here
     return (
       <div>
         {this.props.tickets.map(user => {
@@ -24,19 +110,29 @@ class TicketDetails extends Component {
             return null;
           }
         })}
+        {console.log(risk)}
+        {risk <= 5 ? (
+          <h2 className="safe">Risk: {minRisk}%</h2>
+        ) : risk >= 95 ? (
+          <h2 className="danger">Risk: {maxRisk}%</h2>
+        ) : risk > 5 && risk <= 35 ? (
+          <h2 className="safe">Risk: {risk}%</h2>
+        ) : risk > 35 && risk <= 75 ? (
+          <h2 className="moderate">Risk: {risk}%</h2>
+        ) : (
+          <h2 className="danger">Risk: {risk}%</h2>
+        )}
 
-        <h2>Risk: 5%</h2>
         {/* {console.log(this.props)} */}
         {this.props.tickets.map(user => {
           return user.tickets.map(ticket => {
             // console.log("GET THIS FAR?");
             if (ticket.id == this.props.match.params.ticketId) {
               // console.log("DID I GET THIS FAR?");
-
               return (
                 <div className="ticketDiv">
                   <h2>
-                    <b>€{ticket.price}</b>
+                    <b>Price: €{ticket.price}</b>
                   </h2>
                   <div>
                     <img src={ticket.picture} className="ticketImage" />
@@ -61,7 +157,8 @@ const mapStateToProps = state => {
     userLoggedIn: state.user.token !== null,
     currentUser: state.user,
     events: state.events,
-    tickets: state.ticket
+    tickets: state.ticket,
+    comments: state.comments
   };
 };
 
