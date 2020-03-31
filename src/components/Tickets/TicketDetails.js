3f9316595f6783409c6633c26442311ceb3b204a
Shashank Neelagiri <sneelagiri@outlook.com>
2020-03-31 15:15:45 +0200

    moved risk calculation to back-end

--- src/components/Tickets/TicketDetails.js
+++ src/components/Tickets/TicketDetails.js
@@ -6,105 +6,10 @@ import Comments from "./Comments";
 import { updateTicket } from "../../actions/tickets";
 import { v4 as uuidv4 } from "uuid";
 
-let stateRisk = 0;
-
 class TicketDetails extends Component {
-  componentDidMount() {
-    this.props.dispatch(
-      updateTicket(this.props.match.params.ticketId, stateRisk)
-    );
-  }
-
-  round_to_precision(x, precision) {
-    var y = +x + (precision === undefined ? 0.5 : precision / 2);
-    return y - (y % (precision === undefined ? 1 : +precision));
-  }
-
   render() {
-    // Risk analysis logic stars
-    let numComments = 0;
-    this.props.comments.map(user => {
-      user.comments.map(comment => {
-        if (comment.ticketId === parseInt(this.props.match.params.ticketId)) {
-          numComments++;
-          return comment;
-          // console.log("Number of Comments", numComments);
-        }
-      });
-    });
-    let numTickets = 0;
-    let sumPrice = 0;
-    this.props.tickets.map(user => {
-      return user.tickets.map(ticket => {
-        if (ticket.eventId === parseInt(this.props.match.params.eventId)) {
-          numTickets++;
-          sumPrice = sumPrice + ticket.price;
-          return ticket;
-        } else {
-          return ticket;
-        }
-      });
-    });
     const minRisk = 5;
     const maxRisk = 95;
-    let risk = 0;
-    const averagePrice = sumPrice / numTickets;
-    this.props.tickets.map(user => {
-      return user.tickets.map(ticket => {
-        if (
-          ticket.id === parseInt(this.props.match.params.ticketId) &&
-          user.tickets.length === 1
-        ) {
-          risk = risk + 10;
-          if (numComments > 3) {
-            risk = risk + 5;
-          }
-          const time = parseInt(ticket.updatedAt.slice(11, 13));
-          if (time > 9 && time < 17) {
-            risk = risk - 10;
-          } else {
-            risk = risk + 10;
-          }
-          const priceDifference = ticket.price - averagePrice;
-          if ((priceDifference > 0) & (priceDifference < 10)) {
-            risk = risk - priceDifference;
-          } else if (priceDifference > 10) {
-            risk = risk - 10;
-          } else {
-            risk = risk - priceDifference;
-          }
-          return ticket;
-        } else if (ticket.id === parseInt(this.props.match.params.ticketId)) {
-          if (numComments > 3) {
-            risk = risk + 5;
-          }
-          const time = parseInt(ticket.updatedAt.slice(11, 13));
-          if (time > 9 && time < 17) {
-            risk = risk - 10;
-          } else {
-            risk = risk + 10;
-          }
-
-          const priceDifference = ticket.price - averagePrice;
-          if ((priceDifference > 0) & (priceDifference < 10)) {
-            risk = risk - priceDifference;
-          } else if (priceDifference > 10) {
-            risk = risk - 10;
-          } else {
-            risk = risk - priceDifference;
-          }
-          return ticket;
-        } else {
-          return null;
-        }
-      });
-    });
-    risk = this.round_to_precision(risk, 0.01);
-    stateRisk = risk;
-    // Based on number of comments:
-    // console.log("WHAT IS THE RISK OF THIS?", risk, stateRisk);
-    // console.log(averagePrice);
-    // Risk analysis logic ends here
     return (
       <div>
         {this.props.tickets.map(user => {
@@ -126,27 +31,24 @@ class TicketDetails extends Component {
             return null;
           }
         })}
-        {/* {console.log(risk)} */}
-        {risk <= 5 ? (
-          <h2 className="safe">Risk: {minRisk}%</h2>
-        ) : risk >= 95 ? (
-          <h2 className="danger">Risk: {maxRisk}%</h2>
-        ) : risk > 5 && risk <= 35 ? (
-          <h2 className="safe">Risk: {risk}%</h2>
-        ) : risk > 35 && risk <= 75 ? (
-          <h2 className="moderate">Risk: {risk}%</h2>
-        ) : (
-          <h2 className="danger">Risk: {risk}%</h2>
-        )}
 
-        {/* {console.log(this.props)} */}
         {this.props.tickets.map(user => {
           return user.tickets.map(ticket => {
-            // console.log("GET THIS FAR?");
             if (ticket.id === parseInt(this.props.match.params.ticketId)) {
-              // console.log("DID I GET THIS FAR?");
+              const risk = ticket.risk;
               return (
                 <div className="ticketDiv" key={uuidv4()}>
+                  {risk <= 5 ? (
+                    <h2 className="safe">Risk: {minRisk}%</h2>
+                  ) : risk >= 95 ? (
+                    <h2 className="danger">Risk: {maxRisk}%</h2>
+                  ) : risk > 5 && risk <= 35 ? (
+                    <h2 className="safe">Risk: {risk}%</h2>
+                  ) : risk > 35 && risk <= 75 ? (
+                    <h2 className="moderate">Risk: {risk}%</h2>
+                  ) : (
+                    <h2 className="danger">Risk: {risk}%</h2>
+                  )}
                   <h2>
                     <b>Price: €{ticket.price}</b>
                   </h2>
